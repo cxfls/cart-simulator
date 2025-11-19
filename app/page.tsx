61c9588de43241b691da207ddc9fc017c5e629a7
cxfls <leecxfls@gmail.com>
2025-11-19 15:02:34 +0900

    토스트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import MainPage from "./components/Main";
 
 export default function Home() {
   return (
-    <main className="pt-5">
+    <main className="pt-5 relative">
       <MainPage />
     </main>
   );
